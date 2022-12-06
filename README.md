a70f74ed9c2297208ee084272d8d99b48251c7c1
David Crosson <david.crosson@orange.com>
2022-12-06 06:31:44 +0100

    Add day6 solutions

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # ADVENT OF CODE 2022
 My solutions using scala3, with some coding rules :
-- no side effects
-- no mutable data structure
-- readable, maintainable and easily understandable
+- no side effects,
+- no mutable data structure,
+- readable, maintainable and easily understandable,
+- only with the standard library except for ZIO-test used for unit testing.
 
-Solutions
-- [day01](src/test/scala/day01/Puzzle.scala)
-- [day02](src/test/scala/day02/Puzzle.scala)
-- [day03](src/test/scala/day03/Puzzle.scala)
-- [day04](src/test/scala/day04/Puzzle.scala)
-- [day05](src/test/scala/day05/Puzzle.scala)
-
-Unit tests using ZIO-test.
+Solutions :
+- [day01](src/test/scala/day01/Puzzle.scala) - ok
+- [day02](src/test/scala/day02/Puzzle.scala) - ok
+- [day03](src/test/scala/day03/Puzzle.scala) - ok
+- [day04](src/test/scala/day04/Puzzle.scala) - ok
+- [day05](src/test/scala/day05/Puzzle.scala) - readability should be enhanced
+- [day06](src/test/scala/day05/Puzzle.scala) - ok
---
+++ src/test/scala/day06/Puzzle.scala
@@ -0,0 +1,59 @@
+package day06
+
+import zio.*
+import zio.test.*
+
+def findMarker(input: String, ofLength: Int): Int =
+  input
+    .sliding(ofLength)
+    .zipWithIndex
+    .collect { case (group, index) if group.distinct.size == ofLength => index + ofLength }
+    .next()
+
+// ------------------------------------------------------------------------------
+
+def resolveStar1(input: String): Int = findMarker(input, 4)
+
+// ------------------------------------------------------------------------------
+
+def resolveStar2(input: String): Int = findMarker(input, 14)
+
+// ------------------------------------------------------------------------------
+
+object Puzzle06Test extends ZIOSpecDefault {
+  import zio.nio.file.Path
+  import helpers.Helpers.*
+  val day  = getClass.getName.replaceAll(""".*Puzzle(\d+)Test.*""", "day$1")
+  def spec = suite(s"puzzle $day")(
+    test("star#1") {
+      for {
+        exampleInput <- fileContent(Path(s"data/$day/example-1.txt"))
+        exampleResult = resolveStar1(exampleInput)
+        puzzleInput  <- fileContent(Path(s"data/$day/puzzle-1.txt"))
+        puzzleResult  = resolveStar1(puzzleInput)
+      } yield assertTrue(
+        exampleResult == 7,
+        resolveStar1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5,
+        resolveStar1("nppdvjthqldpwncqszvftbrmjlhg") == 6,
+        resolveStar1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10,
+        resolveStar1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11,
+        puzzleResult == 1287
+      )
+    },
+    test("star#2") {
+      for {
+        exampleInput1 <- fileContent(Path(s"data/$day/example-1.txt"))
+        exampleResult1 = resolveStar2(exampleInput1)
+        puzzleInput   <- fileContent(Path(s"data/$day/puzzle-1.txt"))
+        puzzleResult   = resolveStar2(puzzleInput)
+      } yield assertTrue(
+        exampleResult1 == 19,
+        resolveStar2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23,
+        resolveStar2("nppdvjthqldpwncqszvftbrmjlhg") == 23,
+        resolveStar2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29,
+        resolveStar2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26,
+        puzzleResult == 3716
+      )
+    }
+  )
+}
