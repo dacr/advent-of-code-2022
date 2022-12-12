892d6cbf6f7cd5a1aaf42ff17c9f6c6ee4ee1e04
Crosson David <crosson.david@gmail.com>
2022-12-12 18:34:16 +0100

    Optimize

--- src/test/scala/day12/Puzzle.scala
+++ src/test/scala/day12/Puzzle.scala
@@ -11,7 +11,7 @@ case class Coord(x: Int, y: Int)
 case class Cell(height: Char)            extends AnyVal
 case class Grid(cells: Map[Coord, Cell]) extends AnyVal
 case class Area(grid: Grid, origin: Coord, goal: Coord, width: Int, height: Int) {
-  def printRoute(route: List[Coord]): Unit = {
+  def printRoute(route: List[Coord], bestDepth: Map[Coord, Int] = Map.empty): Unit = {
     val routeSet = route.toSet
     val lines    =
       0.until(height)
@@ -22,8 +22,9 @@ case class Area(grid: Grid, origin: Coord, goal: Coord, width: Int, height: Int)
               val ch    = grid.cells(coord).height
               if (routeSet.contains(coord))
                 s"$RED$BOLD$ch$RESET"
-              else
-                s"$ch"
+              else if (bestDepth.contains(coord))
+                s"$BLUE$ch$RESET"
+              else s"$ch"
             }
             .mkString
         )
@@ -47,37 +48,42 @@ def parse(input: List[String]): Area = {
 def heightCheck(from: Int, to: Int): Boolean = (to - from <= 1) //|| (to - from == 0)
 
 @annotation.tailrec
-def walk(area: Area, visitQueue: List[(List[Coord], Int)], bestDepths: Map[Coord, Int]): Int = {
+def shortestPath(area: Area, visitQueue: List[(List[Coord], Set[Coord], Int)], bestDepths: Map[Coord, Int]): List[Coord] = {
   visitQueue match {
-    case Nil => 0
+    case Nil => Nil
 
-    case (current :: path, depth) :: _ if current == area.goal =>
-      //area.printRoute(current :: path)
-      path.size
+    case (current :: path, visited, depth) :: _ if current == area.goal =>
+      // area.printRoute(current :: path, bestDepths)
+      path
 
-    case (from :: path, fromDepth) :: others if bestDepths.contains(from) && fromDepth >= bestDepths(from) =>
-      walk(area, others, bestDepths)
+    case (from :: path, visited, fromDepth) :: others if bestDepths.contains(from) && fromDepth >= bestDepths(from) =>
+      shortestPath(area, others, bestDepths)
 
-    case (from :: path, fromDepth) :: others =>
+    case (from :: path, visited, fromDepth) :: others =>
       val nextToVisit       = List(
         from.copy(x = from.x + 1),
         from.copy(x = from.x - 1),
         from.copy(y = from.y + 1),
         from.copy(y = from.y - 1)
       ).filter { to =>
+        !visited.contains(to) &&
         area.grid.cells.contains(to) &&
-        !path.contains(to) &&
         heightCheck(area.grid.cells(from).height, area.grid.cells(to).height)
       }
       val updatedBestDepths = bestDepths + (from -> fromDepth)
-      val updatedVisitQueue = others ++ nextToVisit.map(to => (to :: from :: path, fromDepth + 1))
-      walk(area, updatedVisitQueue, updatedBestDepths)
+      val updatedVisited    = visited + from
+      val updatedPath       = from :: path
+      val updatedVisitQueue = others ++ nextToVisit.map(to => (to :: updatedPath, updatedVisited, fromDepth + 1))
+      // area.printRoute(from :: path, updatedBestDepths)
+      shortestPath(area, updatedVisitQueue, updatedBestDepths)
   }
 }
 
 def resolveStar1(input: List[String]): Int =
   val area = parse(input)
-  walk(area, (area.origin :: Nil, 0) :: Nil, Map.empty)
+  val path = shortestPath(area, (area.origin :: Nil, Set.empty, 0) :: Nil, Map.empty)
+  // area.printRoute(path)
+  path.size
 
 // ------------------------------------------------------------------------------
 
@@ -85,9 +91,11 @@ def resolveStar2(input: List[String]): Int =
   val area = parse(input)
   area.grid.cells
     .collect { case (pos, cell) if cell.height == 'a' => pos }
-    .map(startPos => walk(area, (startPos :: Nil, 0) :: Nil, Map.empty))
-    .filter(_ > 0)
-    .min
+    .map(startPos => shortestPath(area, (startPos :: Nil, Set.empty, 0) :: Nil, Map.empty))
+    .filter(_.size > 0)
+    .minBy(_.size)
+    // .tap(foundPath => area.printRoute(foundPath))
+    .size
 
 // ------------------------------------------------------------------------------
 
