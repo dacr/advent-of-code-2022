71bb52fafc364aea6d4b4c805fef7f2f0395f2c9
dcr <crosson.david@gmail.com>
2022-12-25 23:23:51 +0100

    Solve day17 first star

--- src/test/scala/day17/Puzzle.scala
+++ src/test/scala/day17/Puzzle.scala
@@ -11,7 +11,11 @@ enum Direction {
   case Right
 }
 
-case class Coord(x: Int, y: Int)
+case class Coord(x: Int, y: Int) {
+  def right = copy(x = x + 1)
+  def left  = copy(x = x - 1)
+  def down  = copy(y = y - 1)
+}
 
 case class RelativeCoord(x: Int, y: Int) {
   def from(coord: Coord): Coord = Coord(x + coord.x, y + coord.y)
@@ -19,13 +23,18 @@ case class RelativeCoord(x: Int, y: Int) {
 
 case class Rock(shape: Set[RelativeCoord]) {
   def coords(leftBottom: Coord) = shape.map(_.from(leftBottom))
+  val width                     = {
+    val xs = shape.map(_.x)
+    xs.max - xs.min + 1
+  }
 }
 
+// of course a faster implementation would be to use an binary approach
 object Rock {
   val rocks = Vector(
     Rock(Set(RelativeCoord(0, 0), RelativeCoord(1, 0), RelativeCoord(2, 0), RelativeCoord(3, 0))),
     Rock(Set(RelativeCoord(1, 0), RelativeCoord(1, 1), RelativeCoord(1, 2), RelativeCoord(0, 1), RelativeCoord(2, 1))),
-    Rock(Set(RelativeCoord(2, 0), RelativeCoord(2, 1), RelativeCoord(2, 2), RelativeCoord(1, 2), RelativeCoord(0, 2))),
+    Rock(Set(RelativeCoord(0, 0), RelativeCoord(1, 0), RelativeCoord(2, 0), RelativeCoord(2, 1), RelativeCoord(2, 2))),
     Rock(Set(RelativeCoord(0, 0), RelativeCoord(0, 1), RelativeCoord(0, 2), RelativeCoord(0, 3))),
     Rock(Set(RelativeCoord(0, 0), RelativeCoord(1, 0), RelativeCoord(1, 1), RelativeCoord(0, 1)))
   )
@@ -45,14 +54,44 @@ def parse(input: String) =
 def simulate(roundLimit: Int, instructions: IndexedSeq[Direction]): Int = {
   @annotation.tailrec
   def worker(roundIndex: Int, instructionIndex: Int, rockLeftBottom: Coord, occupied: Set[Coord]): Int = {
-    val fallingRock = Rock.rocks(roundIndex)
-    val updatedX = instructions(instructionIndex % instructions.size) match {
-      case Direction.Right =>
-      case Direction.Left =>
+    if (roundIndex >= roundLimit) occupied.map(_.y).max
+    else {
+      val fallingRock = Rock.rocks(roundIndex % Rock.rocks.length)
+
+      val updatedX = instructions(instructionIndex % instructions.size) match {
+        case Direction.Right if rockLeftBottom.x + fallingRock.width < 7 =>
+          val rr = rockLeftBottom.right
+          if (fallingRock.coords(rr).intersect(occupied).isEmpty) rr else rockLeftBottom
+
+        case Direction.Left if rockLeftBottom.x > 0 =>
+          val rl = rockLeftBottom.left
+          if (fallingRock.coords(rl).intersect(occupied).isEmpty) rl else rockLeftBottom
+
+        case _ => rockLeftBottom
+      }
+
+      val updatedY = updatedX.down
+      if (fallingRock.coords(updatedY).intersect(occupied).isEmpty)
+        worker(roundIndex, instructionIndex + 1, updatedY, occupied)
+      else {
+        val updatedOccupied = occupied ++ fallingRock.coords(updatedX)
+        val highestY        = updatedOccupied.map(_.y).max
+        //show(occupied)
+        worker(roundIndex + 1, instructionIndex + 1, Coord(2, highestY + 4), updatedOccupied)
+      }
     }
-    ???
   }
-  worker(0, 0, Coord(2, 3), Set.empty)
+  worker(0, 0, Coord(2, 4), 0.to(7).map(x => Coord(x, 0)).toSet)
+}
+
+def show(coords: Set[Coord]): Unit = {
+  val highestY = coords.map(_.y).max
+  highestY.to(0, -1).foreach { y =>
+    println(
+      0.to(6).map(x => if (coords.contains(Coord(x, y))) "#" else ".").mkString
+    )
+  }
+  println("-------")
 }
 
 def resolveStar1(input: String): Int =
@@ -79,7 +118,7 @@ object Puzzle17Test extends ZIOSpecDefault {
         puzzleResult  = resolveStar1(puzzleInput)
       } yield assertTrue(
         exampleResult == 3068,
-        puzzleResult == 0
+        puzzleResult == 3163
       )
     },
     test("star#2") {
