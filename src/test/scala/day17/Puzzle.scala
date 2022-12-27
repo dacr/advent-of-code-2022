2b225880e355313c12e88f3719aaf4f7a5e7bf45
dcr <crosson.david@gmail.com>
2022-12-27 12:33:04 +0100

    Solve Day17 star2

--- src/test/scala/day17/Puzzle.scala
+++ src/test/scala/day17/Puzzle.scala
@@ -24,9 +24,15 @@ case class RelativeCoord(x: Int, y: Int) {
 
 case class Rock(shape: Set[RelativeCoord]) {
   def coords(leftBottom: Coord) = shape.map(_.from(leftBottom))
-  val width                     = {
+
+  val width = {
     val xs = shape.map(_.x)
     xs.max - xs.min + 1
+  }
+
+  val height = {
+    val ys = shape.map(_.y)
+    ys.max - ys.min + 1
   }
 }
 
@@ -71,14 +77,17 @@ def simulate(roundLimit: Long, instructions: IndexedSeq[Direction]): Long = {
         case _ => rockLeftBottom
       }
 
-      val updatedY          = updatedX.down
+      val updatedY = updatedX.down
       if (fallingRock.coords(updatedY).intersect(occupied).isEmpty)
         worker(roundIndex, instructionIndex + 1, currentHighestY, updatedY, occupied)
       else {
         val fallingRockCoords = fallingRock.coords(updatedX)
-        val updatedOccupied = occupied ++ fallingRockCoords
-        val updatedHighestY = max(fallingRockCoords.maxBy(_.y).y, currentHighestY)
-        println(s"$roundIndex $updatedHighestY")
+        // val updatedHighestY   = max(fallingRockCoords.maxBy(_.y).y, currentHighestY)
+        val updatedHighestY   = max(updatedX.y + fallingRock.height - 1, currentHighestY)
+        val updatedOccupied   = if (roundIndex % 1000 == 0) {
+          occupied.filter(_.y > updatedHighestY - 100) ++ fallingRockCoords
+        } else occupied ++ fallingRockCoords
+
         worker(roundIndex + 1, instructionIndex + 1, updatedHighestY, Coord(2, updatedHighestY + 4), updatedOccupied)
       }
     }
@@ -92,9 +101,75 @@ def resolveStar1(input: String): Long =
 
 // ------------------------------------------------------------------------------
 
+def computeDeltas(roundLimit: Long, instructions: IndexedSeq[Direction]): List[Long] = {
+  @annotation.tailrec
+  def worker(roundIndex: Long, instructionIndex: Int, currentHighestY: Long, rockLeftBottom: Coord, occupied: Set[Coord], Δlist: List[Long]): List[Long] = {
+    if (roundIndex >= roundLimit) Δlist.reverse
+    else {
+      val fallingRock = Rock.rocks((roundIndex % Rock.rocks.length).toInt)
+
+      val updatedX = instructions(instructionIndex % instructions.size) match {
+        case Direction.Right if rockLeftBottom.x + fallingRock.width < 7 =>
+          val rr = rockLeftBottom.right
+          if (fallingRock.coords(rr).intersect(occupied).isEmpty) rr else rockLeftBottom
+
+        case Direction.Left if rockLeftBottom.x > 0 =>
+          val rl = rockLeftBottom.left
+          if (fallingRock.coords(rl).intersect(occupied).isEmpty) rl else rockLeftBottom
+
+        case _ => rockLeftBottom
+      }
+
+      val updatedY = updatedX.down
+      if (fallingRock.coords(updatedY).intersect(occupied).isEmpty)
+        worker(roundIndex, instructionIndex + 1, currentHighestY, updatedY, occupied, Δlist)
+      else {
+        val fallingRockCoords = fallingRock.coords(updatedX)
+        // val updatedHighestY   = max(fallingRockCoords.maxBy(_.y).y, currentHighestY)
+        val updatedHighestY   = max(updatedX.y + fallingRock.height - 1, currentHighestY)
+        val updatedOccupied   = if (roundIndex % 1000 == 0) {
+          occupied.filter(_.y > updatedHighestY - 100) ++ fallingRockCoords
+        } else occupied ++ fallingRockCoords
+
+        val Δ = updatedHighestY - currentHighestY
+
+        worker(roundIndex + 1, instructionIndex + 1, updatedHighestY, Coord(2, updatedHighestY + 4), updatedOccupied, Δ :: Δlist)
+      }
+    }
+  }
+  worker(0, 0, 0L, Coord(2, 4), 0.to(7).map(x => Coord(x, 0)).toSet, Nil)
+}
+
+case class Cycle(from: Int, len: Int, pattern: List[Long])
+
+// not the fastest algorithm of course...
+def searchCycle(values: List[Long], maxCycleLength: Int = 100, maxStartDelta: Int = 100, cycleSamples: Int = 10): Option[Cycle] = {
+  LazyList
+    .from(1)
+    .take(maxCycleLength)
+    .flatMap { case cycleLength =>
+      LazyList.from(0).take(maxStartDelta).flatMap { case from =>
+        values.drop(from).grouped(cycleLength).take(cycleSamples).distinct.toList match {
+          case cycle :: Nil => Some(Cycle(from, cycle.length, cycle))
+          case _            => None
+        }
+      }
+    }
+    .headOption
+}
+
 def resolveStar2(input: String): Long =
   val instructions = parse(input)
-  simulate(1_000_000_000_000L, instructions)
+  val upTo         = 1_000_000_000_000L
+  val sample       = computeDeltas(50_000L, instructions)
+  searchCycle(values = sample, maxCycleLength = 2000, maxStartDelta = 200, cycleSamples = 4) match {
+    case None        => 0L
+    case Some(cycle) =>
+      val beforeCycleCount = cycle.from
+      sample.take(beforeCycleCount).sum +
+        cycle.pattern.sum * ((upTo - beforeCycleCount) / cycle.len) +
+        cycle.pattern.take(((upTo - beforeCycleCount) % cycle.len).toInt).sum
+  }
 
 // ------------------------------------------------------------------------------
 
@@ -122,7 +197,7 @@ object Puzzle17Test extends ZIOSpecDefault {
         puzzleResult   = resolveStar2(puzzleInput)
       } yield assertTrue(
         exampleResult1 == 1514285714288L,
-        puzzleResult == 0L
+        puzzleResult == 1560932944615L
       )
     }
   ) @@ timed @@ sequential
